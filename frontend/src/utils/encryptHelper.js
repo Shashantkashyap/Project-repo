f3e030ef9cd1903c43f23228c715863c456d1288
Shashant Kashyap <shashant.kashyap999@gmail.com>
2025-07-16 10:30:58 +0530

    Delete frontend directory

--- frontend/src/utils/encryptHelper.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-export function encryptSectionData(sectionData, secretKey) {
-  const encrypted = CryptoJS.AES.encrypt(
-    JSON.stringify(sectionData),
-    secretKey
-  ).toString();
-  return encrypted;
-}
